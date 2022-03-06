be533b82d906562db1a54a54b19e2acb5d2236c2
Adarsh Chourasia <101025480+adxrsh00@users.noreply.github.com>
2022-03-07 05:28:09 +0530

    Update Sum till natural number.cpp

--- Sum till natural number.cpp
+++ Sum till natural number.cpp
@@ -11,3 +11,25 @@ int main()
         }
         cout<< sum;
 }
+
+////////////////////// using recursion ////////////////////// 
+
+
+#include <iostream>
+
+using namespace std;
+int sum(int n)
+{
+    if(n==1)
+    return 1;
+    return((n)+sum(n-1));
+}
+int main()
+{
+      int n;
+    cin>>n;
+    cout<<sum(n);
+}
+    
+
+   
