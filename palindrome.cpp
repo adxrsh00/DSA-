8cba9e0a31cc7c44dd18b3b60a386510704f18de
Adarsh Chourasia <101025480+adxrsh00@users.noreply.github.com>
2022-03-07 05:18:20 +0530

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -5,38 +5,27 @@ using namespace std;
 
 int main()
 {
-    int n,i,m,a,c=0;
+    int n,i,m,a,c=0,nn;
     cin >>n;// expecting user input to be less than or a 10 digit number
     m=n;
     int ar[10];
     for(i=0;i<10;i++)
-    {           
+    {   
+        
         a=m%10;
+        if(a==0)
+        {
+        break;
+        }
         ar[i]=a;
         m=m/10;
-        if(m>0)
-        {
-            c++;//counter to check number of digits
-        }
-        else
-        {
-        c++;
-        
-        break;
-    }}
-    
-    int nn=0,x=c-1;//new number
-     for(i=0;i<c;i++)
-    {
-      nn+=ar[i]*(pow(10,x));// extracting reversed number
-       x--;
-    }
-    if(nn==n)
-    {
+        nn=nn*10+a;
+      if(nn==n)
+       {
         cout<<"Palindrome Number";
-    }
-    else
-    cout<<"Not a Palindrome Number";
+       }
+       else
+          cout<<"Not a Palindrome Number";
     
     return 0;
 }
