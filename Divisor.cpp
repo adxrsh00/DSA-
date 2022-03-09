32e63f2d63746e6a236a73a8f8cd21b2853dfdcd
Adarsh Chaurasia <101025480+adxrsh00@users.noreply.github.com>
2022-03-09 05:40:02 +0530

    Create Divisor.cpp

---
+++ Divisor.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void Divisor(int a)
+{
+    int i;
+    cout<<"The Divisors of the number are : ";
+    for(i=1;i<=a;i++)
+    {
+        if(a%i==0)
+        {
+            cout<<i<<" ";
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cout<<"input the number : ";
+    cin>>n;
+    Divisor(n);
+    return 0;
+}
