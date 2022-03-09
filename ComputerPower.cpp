a9aebc1d64d1e8c7775749d393352e78d54b8850
Adarsh Chaurasia <101025480+adxrsh00@users.noreply.github.com>
2022-03-09 07:02:44 +0530

    Create ComputerPower.cpp

---
+++ ComputerPower.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int power(int x, int n)
+{
+    if(n==0)
+    return 1;
+    
+    int temp = power(x,n/2);
+    temp=temp*temp;
+    if(n%2==0)
+        return temp;
+    else
+        return temp*x;
+}
+
+int main()
+{
+    int n,x;
+    cout<<"input the number : ";
+    cin>>x;
+    cout<<"input the power  : ";
+    cin>>n;
+    cout<<power(x,n);
+    return 0;
+}
